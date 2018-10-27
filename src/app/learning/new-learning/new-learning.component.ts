bef3d21f4a63658a4db3e62b5889ded04c447103
Andrii Savchuk <andriksr@gmail.com>
2018-10-27 18:42:28 -0400

    Added card to the New Learning section

--- src/app/learning/new-learning/new-learning.component.ts
+++ src/app/learning/new-learning/new-learning.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tasks {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-new-learning',
   templateUrl: './new-learning.component.html',
   styleUrls: ['./new-learning.component.css']
 })
 export class NewLearningComponent implements OnInit {
+  selectedValue: string;
+
+  tasks: Tasks[] = [
+    {value: 'algorithms-0', viewValue: 'Algorithms'},
+    {value: 'data-1', viewValue: 'Data Structures'},
+    {value: 'java-2', viewValue: 'Java'}
+  ];
 
   constructor() { }
 
