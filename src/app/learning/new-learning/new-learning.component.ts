a6aeb1b488381ad013c426d249208cef86953e43
Andrii Savchuk <andriksr@gmail.com>
2018-12-24 13:27:09 -0500

    Get data using Snapshot changes

--- src/app/learning/new-learning/new-learning.component.ts
+++ src/app/learning/new-learning/new-learning.component.ts
@@ -3,7 +3,8 @@ import { NgForm } from '@angular/forms';
 import { LearningService } from '../learning.service';
 import { Exercise } from '../exercise.model';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-learning',
@@ -22,7 +23,23 @@ export class NewLearningComponent implements OnInit {
 
   ngOnInit() {
     // this.exercises = this.learningService.getAvailableExercises();
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    // this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            ...doc.payload.doc.data()
+          };
+        });
+      }));
+      // .subscribe(result => {
+      //   for (const res of result) {
+      //     console.log(result);
+      //   }
+      // });
   }
 
   onStartLearning(form: NgForm) {
