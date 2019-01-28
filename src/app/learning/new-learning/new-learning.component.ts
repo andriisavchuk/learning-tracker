e0534548e84111d27a0a16988a6d18d6d93eabb8
Andrii Savchuk <andriksr@gmail.com>
2019-01-28 15:42:15 -0500

    refactor code. move fetching data from new-learning component to the learning service

--- src/app/learning/new-learning/new-learning.component.ts
+++ src/app/learning/new-learning/new-learning.component.ts
@@ -1,51 +1,33 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LearningService } from '../learning.service';
 import { Exercise } from '../exercise.model';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-learning',
   templateUrl: './new-learning.component.html',
   styleUrls: ['./new-learning.component.css']
 })
-export class NewLearningComponent implements OnInit {
-  // @Output() learningStart = new EventEmitter<void>();
-  // exercises: Exercise[] = [];
-  exercises: Observable<Exercise[]>;
+export class NewLearningComponent implements OnInit, OnDestroy {
+  exercises: Exercise[];
+  exerciseSubscription: Subscription;
 
-  constructor(
-    private learningService: LearningService,
-    private db: AngularFirestore
-  ) {}
+  constructor(private learningService: LearningService) {}
 
   ngOnInit() {
-    // this.exercises = this.learningService.getAvailableExercises();
-    // this.exercises = this.db.collection('availableExercises').valueChanges();
-    this.exercises = this.db
-      .collection('availableExercises')
-      .snapshotChanges()
-      .pipe(
-        map(docArray => {
-          return docArray.map(doc => {
-            return {
-              id: doc.payload.doc.id,
-              ...doc.payload.doc.data()
-            } as Exercise;
-          });
-        })
-      );
-    // .subscribe(result => {
-    //   for (const res of result) {
-    //     console.log(res.payload.doc.data());
-    //   }
-    // });
+    this.exerciseSubscription = this.learningService.exercisesChanged.subscribe(
+      exercises => (this.exercises = exercises)
+    );
+    this.learningService.getAvailableExercises();
   }
 
   onStartLearning(form: NgForm) {
-    // this.learningStart.emit();
     this.learningService.startExercise(form.value.exercise);
   }
+
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
+  }
+
 }
